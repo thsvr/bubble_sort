eb5f9cc8493152473effa6f5aea43bd072d5f41c
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-09-26 19:48:26 -0300

    linters in process 1.2

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,19 +1,15 @@
 # frozen_string_literal: true
-def bubble_sort arr
 
-    elem= arr.length 
-
-    loop do 
-
-        sorted = false #when to break the loop
-
+def bubble_sort(arr)
+elem= arr.length 
+    loop do
+    sorted = false #when to break the loop
         (elem-1).times do |x|
-            if arr[x] > arr[x+1]
-                arr[x], arr[x+1] = arr[x+1], arr[x]
-            sorted = true
+        if arr[x] > arr[x+1]
+            arr[x], arr[x+1] = arr[x+1], arr[x]
+            sorted= true
             end
         end
-
         break if sorted == false
     end
     return arr
@@ -22,16 +18,13 @@ end
 #test
 #y= [4,3,78,2,0,2]
 #puts bubble_sort(y) 
-
-
-def bubble_sort_by arr
-    elem= arr.length 
-
+def bubble_sort_by(arr)
+elem= arr.length 
     loop do
-        sorted = false
+    sorted = false
         (elem-1).times do |x|
-            if yield(arr[x], arr[x+1]).negative?
-                arr[x], arr[x+1] = arr[x+1], arr[x]
+        if yield(arr[x], arr[x+1]).negative?
+            arr[x], arr[x+1] = arr[x+1], arr[x]
             sorted = true
             end
         end
@@ -39,10 +32,5 @@ def bubble_sort_by arr
     end
     arr
 end
-   
-
 puts bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}
-
-
 # ["hi", "hey", "hello"]
-  
