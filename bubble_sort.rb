a7be229f6aba6bc6f715b3451d7adc67b6b25cb0
thsvr <vr.ths.zd@gmail.com>
2019-09-26 18:24:17 -0300

    second method

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -6,7 +6,7 @@ def bubble_sort arr
         sorted = false #when to break the loop
 
         (elem-1).times do |x|
-            if arr[x] > arr[x+1]
+            if arr[x] > arr[x+1] 
                 arr[x], arr[x+1] = arr[x+1], arr[x]
             sorted = true
             end
@@ -18,5 +18,33 @@ def bubble_sort arr
 end
 
 #test
-y= [1,3,2,4,6,8,1,2,9,3]
-puts bubble_sort(y)
+#puts y= [4,3,78,2,0,2]
+#puts bubble_sort(y)
+
+def bubble_sort_by arr
+    elem= arr.length 
+
+    loop do 
+
+        sorted = false
+
+        (elem-1).times do |x|
+            if yield(arr[x] > arr[x+1]).positive?
+                arr[x], arr[x+1] = arr[x+1], arr[x]
+            sorted = true
+            end
+        end
+
+        break if sorted == false
+    end
+    arr
+end
+   
+
+ 
+bubble_sort_by(["hi","hello","hey"]) do |left,right|
+left.length - right.length
+end
+=> ["hi", "hey", "hello"]
+  
+
