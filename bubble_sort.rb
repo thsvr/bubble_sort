cf8e48bb4005447ed24eb7b202d33e285ba9c55a
sofia barrantes gamboa <sofiabarrantes.gamboa@gmail.com>
2019-09-27 12:56:07 -0300

    fix bug

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,36 +1,36 @@
 # frozen_string_literal: true
 
 def bubble_sort(arr)
-elem= arr.length 
-    loop do
-    sorted = false #when to break the loop
-        (elem-1).times do |x|
-        if arr[x] > arr[x+1]
-            arr[x], arr[x+1] = arr[x+1], arr[x]
-            sorted= true
-        end
-        end
-        break if sorted == false
+  elem = arr.length
+  loop do
+    sorted = false # when to break the loop
+    (elem - 1).times do |x|
+      if arr[x] > arr[x + 1]
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        sorted = true
+      end
     end
-    return arr
+    break if sorted == false
+  end
+  arr
 end
 
-#test
-#y= [4,3,78,2,0,2]
-#puts bubble_sort(y) 
+# test
+# y= [4,3,78,2,0,2]
+# puts bubble_sort(y)
 def bubble_sort_by(arr)
-elem= arr.length 
-    loop do
+  elem = arr.length
+  loop do
     sorted = false
-        (elem-1).times do |x|
-        if yield(arr[x], arr[x+1]).negative?
-            arr[x], arr[x+1] = arr[x+1], arr[x]
-            sorted = true
-        end
-        end
-        break if sorted == false
+    (elem - 1).times do |x|
+      if yield(arr[x], arr[x + 1]).negative?
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        sorted = true
+      end
     end
-    arr
+    break if sorted == false
+  end
+  arr
 end
-puts bubble_sort_by(["hi","hello","hey"]) {|left,right| left.length - right.length}
+puts bubble_sort_by(%w[hi hello hey]) { |left, right| left.length - right.length }
 # ["hi", "hey", "hello"]
