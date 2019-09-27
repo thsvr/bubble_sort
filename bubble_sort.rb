a45fb3818da133816383f1cfeb1ebfa38fc032cf
thsvr <vr.ths.zd@gmail.com>
2019-09-27 13:11:04 -0300

    rubocop fixed

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -3,7 +3,6 @@
 def bubble_sort(arr)
   elem = arr.length
   loop do
-
     sorted = true # when to break the loop
 
     (elem - 1).times do |x|
@@ -18,21 +17,21 @@ def bubble_sort(arr)
 end
 
 # test
- y= [4,3,78,2,0,2]
- puts bubble_sort(y)
+y = [4, 3, 78, 2, 0, 2]
+puts bubble_sort(y)
 
 def bubble_sort_by(arr)
   elem = arr.length
 
   loop do
     sorted = true
-        (elem-1).times do |x|
-        if yield(arr[x], arr[x+1]).positive?
-            arr[x], arr[x+1] = arr[x+1], arr[x]
-            sorted = false
-        end
-        end
-        break if sorted == true
+    (elem - 1).times do |x|
+      if yield(arr[x], arr[x + 1]).positive?
+        arr[x], arr[x + 1] = arr[x + 1], arr[x]
+        sorted = false
+      end
+    end
+    break if sorted == true
   end
   arr
 end
